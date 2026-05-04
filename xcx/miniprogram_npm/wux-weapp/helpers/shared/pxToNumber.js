27ddccdc6376e0e5d6a1e5911026d87da8d7eb26
zmhao520 <2938307109@qq.com>
2026-05-04 16:16:41 +0800

    chore: remove miniprogram_npm from git tracking

--- xcx/miniprogram_npm/wux-weapp/helpers/shared/pxToNumber.js
+++
@@ -1,1 +0,0 @@
-"use strict";function pxToNumber(e){if(!e)return 0;if("number"==typeof e)return e;var r=e.match(/^\d*(\.\d*)?/);return r?Number(r[0]):0}Object.defineProperty(exports,"__esModule",{value:!0}),exports.pxToNumber=pxToNumber;
--- xcx/miniprogram_npm/wux-weapp/loading/utils.js
+++
@@ -1,1 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var defaults={prefixCls:"wux-loading",classNames:"wux-animate--fadeIn",text:"数据加载中",mask:!(exports.defaults=void 0),transparent:!0};exports.defaults=defaults;
